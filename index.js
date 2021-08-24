ef3f8843ec0b07014b7df0e0c8f5afd234266e04
Haninayue <haninaidayu@gmail.com>
2021-08-24 08:14:04 +0800

    Revert "Deleted everything"
    This reverts commit c46607e2f3eef88928c084a4dfcba9ec677f629d.

---
+++ index.js
@@ -0,0 +1,8 @@
+// Working with APIs
+
+// We need to answer the following questions
+
+// What is the endpoint (URL)
+// What query string/parameters do we need to provide
+// What required parameters?
+// What optional parameters do we want to add?
